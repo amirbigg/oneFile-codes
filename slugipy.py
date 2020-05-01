44dd02e8166aca64b1e45c67f26ce01ccd063e8d
amirhossein bigdelu <amirbig44@gmail.com>
2020-05-01 19:41:14 +0430

    add slugipy

---
+++ slugipy.py
@@ -0,0 +1,29 @@
+class Slugipy:
+    def __init__(self, text, lower=None, separator='-'):
+        self.text = text
+        self.lower = lower
+        self.separator = separator
+
+    def slug(self):
+        self.validate(self.text)
+        if self.lower:
+            self.text = self.to_lower(self.text)
+        value = self.separate(self.text)
+        return value
+
+    def validate(self, value):
+        if not isinstance(value, str):
+            raise TypeError('value must be a string')
+        return True
+
+    def separate(self, value):
+        value = str(value).split(' ')
+        value = f'{self.separator}'.join(value)
+        return value
+
+    def to_lower(self, value):
+        return value.lower()
+
+
+s = Slugipy('سلام به همه', lower=True)
+print(s.slug())
